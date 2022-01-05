2a25f1d6f6b2bd0b3878db28e138aa4891332e9f
Rassniff <66079622+Rassniff@users.noreply.github.com>
2022-01-05 16:55:25 +0100

    seconda modifica

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/indexstyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Biblioteca</title>
+</head>
+
+<body>
+    <div class="main">
+        <div class="navbar">
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Libri</a>
+                <a href="#">Prestito</a>
+                <div class="animation start-home"></div>
+            </nav>
+        </div>
+
+
+
+
+    </div>
+
+</body>
+
+</html>
