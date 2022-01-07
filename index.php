977eaa8bb92570ce692ddc18f2118c39e424028a
E-sc-D <scorzaedoardo@gmail.com>
2022-01-07 12:09:31 +0100

    implementazione barra index

--- index.php
+++ index.php
@@ -6,23 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/indexstyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Biblioteca</title>
+    <title>BIBLIOTECA BELLUZZI-DA VINCI</title>
 </head>
 
 <body>
     <div class="main">
-        <div class="title">
-            <h1>BIBLIOTECA BELLUZZI-DA-VINCI</h1>
-        </div>
-        <div class="navbar">
+       
+   <!-- <div class="navbar">
             <nav class="barra">
                 <a href="index.php">Home</a>
                 <a href="#">Libri</a>
                 <a href="#">Prestito</a>
                 <div class="animation start-home"></div>
-            </nav>
-        </div>
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,600,700&display=swap" rel="stylesheet">
+            </nav>       
+        </div>-->
         <div class="layout">
             <input name="nav" type="radio" class="nav home-radio" id="home" checked="checked" />
             <div class="page home-page">
@@ -43,9 +40,38 @@
 
             <input name="nav" type="radio" class="about-radio" id="about" />
             <div class="page about-page">
-                <div class="page-contents">
-                    <h2>About</h2>
-                    <p>Amet consectetur adipisicing elit. Sed ipsam ad exercitationem, quo quae ullam, quidem laudantium corporis quis minima debitis nesciunt repellat. Quos dolore ex quis voluptas, minus ut?</p>
+                <div class="index">
+                    <button class="button-28" role="button">A</button>
+                    <button class="button-28" role="button">B</button>
+                    <button class="button-28" role="button">C</button>
+                    <button class="button-28" role="button">D</button>
+                    <button class="button-28" role="button">E</button>
+                    <button class="button-28" role="button">F</button>
+                    <button class="button-28" role="button">G</button>
+                    <button class="button-28" role="button">H</button>
+                    <button class="button-28" role="button">I</button>
+                    <button class="button-28" role="button">J</button>
+                    <button class="button-28" role="button">K</button>
+                    <button class="button-28" role="button">L</button>
+                    <button class="button-28" role="button">M</button>
+                    <button class="button-28" role="button">N</button>
+                    <button class="button-28" role="button">O</button>
+                    <button class="button-28" role="button">P</button>
+                    <button class="button-28" role="button">Q</button>
+                    <button class="button-28" role="button">R</button>
+                    <button class="button-28" role="button">S</button>
+                    <button class="button-28" role="button">T</button>
+                    <button class="button-28" role="button">U</button>
+                    <button class="button-28" role="button">V</button>
+                    <button class="button-28" role="button">W</button>
+                    <button class="button-28" role="button">X</button>
+                    <button class="button-28" role="button">Y</button>
+                    <button class="button-28" role="button">Z</button>
+
+                    <p>hello</p>
+                </div>
+                <div class="listofbooks">
+                    <p>hello</p>
                 </div>
             </div>
             <label class="nav" for="about">
@@ -55,7 +81,7 @@
                         <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
                         <line x1="12" y1="17" x2="12" y2="17"></line>
                     </svg>
-                    About
+                    Libri
                 </span>
             </label>
 
@@ -71,7 +97,7 @@
                     <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
                         <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
                     </svg>
-                    Contact
+                    Prenotazione
                 </span>
 
             </label>
