8b72a1d3f510c8b1980b34c390ff5dc5192f3631
Rassniff <66079622+Rassniff@users.noreply.github.com>
2022-01-17 21:53:03 +0100

    .

--- libro.php
+++ libro.php
@@ -83,6 +83,7 @@ $libro = mysqli_fetch_array($result,MYSQLI_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/libro.css">
+    <link href="javascript/indexjs.js">
     <title><?php echo $libro["Titolo"];?></title>
 </head>
 <body>
